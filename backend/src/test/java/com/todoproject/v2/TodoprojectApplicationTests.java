0ff5cf758b86110f43d46f551cf45b5f2b8a5244
redoxkali <alperarinc@hotmail.com>
2022-03-30 21:29:00 +0300

    Hazır Frontend

---
+++ backend/src/test/java/com/todoproject/v2/TodoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todoproject.v2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
